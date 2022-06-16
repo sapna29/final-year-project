93ebe9180c074572b46bc7e878f9de60940907d9
sapna29 <sapna.18jics093@jietjodhpur.ac.in>
2022-06-16 10:06:38 +0530

    career guide web application

---
+++ questions.js
@@ -0,0 +1,225 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " If the number 467X4 is divisible by 9, find the value of the digit marked as X.",
+    answer: "6",
+    options: [
+      "4",
+      "5",
+      "6",
+      "7"
+    ]
+  },
+    {
+    numb: 2,
+    question: " Find the HCF of 210, 385, and 735?",
+    answer: "35",
+    options: [
+      "7",
+      "14",
+      "21",
+      "35"
+    ]
+  },
+    {
+    numb: 3,
+    question: " Find the largest 4 digit number that is exactly divisible by 66?",
+    answer: "9966",
+    options: [
+      "9987",
+      "9916",
+      "9912",
+      "9966"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the area of a triangle with base 5 meters and height 10 meters?",
+    answer: "25 square meters",
+    options: [
+      "20 square meters",
+      "25 square meters",
+      "35 square meters",
+      "40 square meters"
+    ]
+  },
+    {
+    numb: 5,
+    question: " Find the missing term of the given expression: 18.834 + 818.34 -? = 618.43?",
+    answer: "218.744",
+    options: [
+      "218.744",
+      "217.644",
+      "217.844",
+      "217.744"
+    ]
+  },
+  {
+    numb: 6,
+    question: " Vikas and Mohan working together can complete a work in 6 days. If Vikas alone completes the same work in 10 days, in how many days Mohan alone can complete the same work?",
+    answer: "15 days",
+    options: [
+      "13 days",
+      "14 days",
+      "16 days",
+      "15 days"
+    ]
+  },
+  {
+    numb: 7,
+    question: " If January 1, 1996, was Monday, what day of the week was January 1, 1997?",
+    answer: "Wednesday",
+    options: [
+      "Thursday",
+      "Wednesday",
+      "Friday",
+      "Sunday"
+    ]
+  },
+  {
+    numb: 8,
+    question: " How many times the hands of a clock coincide in a day?",
+    answer: "22",
+    options: [
+      "17",
+      "21",
+      "22",
+      "24"
+    ]
+  },
+
+{
+    numb: 9,
+    question: " 40 % of 280 = ?",
+    answer: "112",
+    options: [
+      "112",
+      "116",
+      "115",
+      "120"
+    ]
+  },
+ 
+  {
+    numb: 10,
+    question: "If a - b = 3 and a2 + b2 = 29, find the value of ab." ,
+    answer: "10",
+    options: [
+      "10",
+      "12",
+      "15",
+      "18"
+    ]
+  },
+  {
+    numb: 11,
+    question: "RQP, ONM, _, IHG, FED, find the missing letters?" ,
+    answer: "LKJ",
+    options: [
+      "CDE",
+      "LKI",
+      "LKJ",
+      "BAC"
+    ]
+  },
+  {
+    numb: 12,
+    question: "PETAL: FLOWER" ,
+    answer: "Engine: Car",
+    options: [
+      "Pen: Paper",
+      "Ball: Game",
+      "Cat: Dog",
+      "Engine: Car"
+    ]
+  },
+  {
+    numb: 13,
+    question: " Pointing to a photograph, a man said, I have no brother, and that man's father is my father's son. Whose photograph was it?" ,
+    answer: "His son",
+    options: [
+      "His own",
+      "His son",
+      "His father",
+      "His nephew"
+    ]
+  },
+  {
+    numb: 14,
+    question: "If in a certain language, NOIDA is coded as OPJEB, how is DELHI coded in that language?" ,
+    answer: "EFMIJ",
+    options: [
+      "CDKGH",
+      "IHLED",
+      "EFMIJ",
+      "FGNJK"
+    ]
+  },
+  {
+    numb: 15,
+    question: " Peter is in the East of Tom and Tom is in the North of John. Mike is in the South of John then in which direction of Peter is Mike?" ,
+    answer: "South-West",
+    options: [
+      "South-West",
+      "South-East",
+      "South",
+      "North-East"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Choose one of the following options that means the opposite of the given word; Luminous: " ,
+    answer: "Dim",
+    options: [
+      "Bright",
+      "soft",
+      "clear",
+      "Dim"
+    ]
+  },
+  {
+    numb: 17,
+    question: "A person who travels on foot. ",
+    answer: "pedestrian",
+    options: [
+      "pedestrian",
+      "traveler",
+      "walker",
+      "health conscious"
+    ]
+  },
+  {
+    numb: 18,
+    question: ".............Himalayas provides protection to India against enemies. " ,
+    answer: "The",
+    options: [
+      "No Articles",
+      "An",
+      "The",
+      "A"
+    ]
+  },
+  {
+    numb: 19,
+    question:"They walked ....... the edge of the land.",
+    answer: "Either A or B",
+    options: [
+      "up to",
+      "until",
+      "Either A or B",
+      "there"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Tick the correct synonym of the given word: Brief" ,
+    answer: "Short",
+    options: [
+      "Partial",
+      "Short",
+      "Limited",
+      "Little"
+    ]
+  },
+];
